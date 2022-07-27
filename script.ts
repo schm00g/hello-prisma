b7b583ad18e07c0195711057fb4ad71d1f147f43
schm00g <hk8igureu@relay.firefox.com>
2022-07-27 14:05:36 +0100

    create user with post

--- script.ts
+++ script.ts
@@ -3,7 +3,18 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  const users = await prisma.user.findMany()
+  const users = await prisma.user.create({
+    data: {
+      name: 'Katy',
+      email: 'katy@prisma.io',
+      posts: {
+        create: {
+          title: 'Hello world',
+          content: 'My first post!'
+        }
+      }
+    }
+  })
   console.log(users);
 }
 
