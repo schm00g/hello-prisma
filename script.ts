ae750476c692fd926116543473bb275f9a99ca73
schm00g <hk8igureu@relay.firefox.com>
2022-07-27 13:49:27 +0100

    fetch all users with findMany()

--- script.ts
+++ script.ts
@@ -3,13 +3,8 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: 'Bonny',
-      email: 'bonny@prisma.io'
-    },
-  })
-  console.log(user);
+  const users = await prisma.user.findMany()
+  console.log(users);
 }
 
 main()
